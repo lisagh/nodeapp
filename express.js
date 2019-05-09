6c11f5d1ca628d00749e9c382d85c4145c6f878f
ththlisa <ththlisa@gmail.com>
2019-05-08 23:21:35 -0500

    test

--- express.js
+++ express.js
@@ -1,6 +1,8 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 const courses = [
   { id: 1, name: "courseA" },
   { id: 2, name: "courseB" },
@@ -15,6 +17,14 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
+  const { error } = ValidateCourse(req.body);
+
+  console.log(error);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -23,12 +33,52 @@ app.post("/api/courses", (req, res) => {
   res.send(course);
 });
 
+app.put("/api/courses/:id", (req, res) => {
+  // look up the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("the given id has not matched course");
+
+  const { error } = ValidateCourse(req.body);
+
+  console.log(error);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
 //create single route
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("the given id has not matched course");
+  if (!course)
+    return res.status(404).send("the given id has not matched course");
   res.send(course);
 });
 
 const port = process.env.port || 4040;
 app.listen(port, () => console.log(`listen to port ${port}`));
+
+function ValidateCourse(course) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(course, schema);
+}
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("the given id has not matched course");
+
+  const index = courses.indexOf(course);
+  //remove an object from the course array
+  courses.splice(index, 1);
+
+  res.send(course);
+});
