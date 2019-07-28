6ecc3bfcb71aa966cf2d06984d0cc02d7650c872
ththlisa <ththlisa@gmail.com>
2019-07-28 00:44:59 -0500

    slideshow

--- buffer.js
+++ buffer.js
@@ -1,4 +1,13 @@
-buf = new Buffer(256);
+buf = new Buffer.alloc(256);
 len = buf.write("Simply Easy Learning");
 
 console.log("Octets written : " + len);
+//The Buffer class was introduced as part of the Node.js API
+//to enable interaction with octet streams in TCP streams,
+//file system operations, and other contexts.
+//more info:
+//https://www.freecodecamp.org/news/do-you-want-a-better-understanding-of-buffer-in-node-js-check-this-out-2e29de2968e8/
+//That “waiting area” is the buffer!
+//It is a small physical location in your computer,
+//usually in the RAM, where data are temporally gathered,
+//wait, and are eventually sent out for processing during streaming.
